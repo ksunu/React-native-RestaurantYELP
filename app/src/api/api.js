e1ab4f8eccfde769ffccd97149eecad1e13519ba
ksunu <soonk@outlook.com>
2021-09-30 10:28:51 +0200

    commit

---
+++ app/src/api/api.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+import { YELP_API_URL, YELP_API_KEY } from "@env";
+
+export default axios.create({
+  baseURL: `${YELP_API_URL}`,
+  headers: {
+    Authorization: `Bearer ${YELP_API_KEY}`
+  }
+});
+
